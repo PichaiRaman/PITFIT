81fb8867057cfe9a7d047648fafe5c3da6cbaa03
PichaiRaman <pichai.raman@gmail.com>
2015-05-31 21:34:11 -0400

    Update aim1.R

--- code/aim1.R
+++ code/aim1.R
@@ -4,9 +4,59 @@
 #
 #Pichai Raman
 #5/31/2015
-##############################i
+##############################
+
+#Read in data
+expData <- read.delim("../data/ovarianData.txt");
+annotData <- read.delim("../data/ovarianDataAnnot.txt");
 
 
+
+SigLimmaTrain <- function(myData, myGene, thresh=.20)
+{
+#pull our vector of values for gene
+myVect <- myData[myGene,];
+myEcdf <- ecdf(myVect);
+myVectECDF <- myEcdf(myVect);
+names(myVectECDF) <- names(myVect);
+
+low <- names(myVectECDF[myVectECDF<thresh]);
+high <- names(myVectECDF[myVectECDF>(1-thresh)]); 
+
+
+#Create targets to run limma
+tmpLow <- data.frame(low, rep("LOW", length(low)))
+colnames(tmpLow) <- c("SAMP", "CLASS");
+
+tmpHigh <- data.frame(high, rep("HIGH", length(high)))
+colnames(tmpHigh) <- c("SAMP", "CLASS");
+
+targets <- rbind(tmpLow, tmpHigh);
+targets[,2] <- as.factor(targets[,2]);
+rownames(targets) <- targets[,1];
+targets <- targets[-1];
+
+fTarget <- factor(targets[,"CLASS"]);
+design <- model.matrix(~0+fTarget);
+colnames(design) <- gsub("fTarget", "", colnames(design));
+
+#Run Limma
+trainDatatmp <- trainData[,rownames(targets)];
+fit <- lmFit(trainDatatmp, design);
+myConts <- c("HIGH-LOW");
+
+
+contrast.matrix <- makeContrasts(contrasts=myConts, levels=design)
+fit2 <- contrasts.fit(fit, contrast.matrix)
+fit2 <- eBayes(fit2)
+
+#Okay now combine all into one result data frame
+tmpLimmaOut <- topTable(fit2, number=60000, 1)[,c("ID", "logFC", "P.Value", "adj.P.Val")];
+tmpLimmaOut <- tmpLimmaOut[abs(tmpLimmaOut[,"logFC"])>1,];
+tmpLimmaOut <- tmpLimmaOut[tmpLimmaOut[,"adj.P.Val"]<.01,];
+
+
+}
 
 
 
