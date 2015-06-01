6ac5cdb62e966e973a3efcc6538ae4b2356f4fb8
PichaiRaman <pichai.raman@gmail.com>
2015-06-01 00:10:21 +0000

    added aim1.R code

---
+++ code/aim1.R
@@ -0,0 +1,20 @@
+##############################
+#Code to support the Aim 1 tool
+#which is for target discovery.
+#
+#Pichai Raman
+#5/31/2015
+##############################i
+
+
+
+
+
+
+
+
+
+
+
+
+
