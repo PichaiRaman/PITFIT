da8416cbea3bb90415821fb89e4d8e40c612fb5f
PichaiRaman <pichai.raman@gmail.com>
2015-11-15 02:50:28 +0000

    added paring code

--- code/aim-1/getTCGA.sh
+++ code/aim-1/getTCGA.sh
@@ -5,37 +5,27 @@ echo "changed directory"
 firehose_get -b -tasks segmented_scna_minus_germline_cnv_hg19__seg.Level_3 Mutation_Packager_Oncotated_Calls.Level_3 Merge_Clinical.Level_1 rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data.Level_3 stddata latest OV PAAD PRAD LIHC READ
 
 #Expand all tar gz
-cd z
-cd OV
-cd *
+cd */OV/*
 for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done
 rm *.md5
 rm *.gz
 cd /srv/shiny-server/PITFIT/data
-cd *
-cd PAAD
-cd *
+cd */PAAD/*
 for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done
 rm *.md5
 rm *.gz
 cd /srv/shiny-server/PITFIT/data
-cd *
-cd PRAD
-cd *
+cd */PRAD/*
 for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done
 rm *.md5
 rm *.gz
 cd /srv/shiny-server/PITFIT/data
-cd *
-cd LIHC
-cd *
+cd */LIHC/*
 for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done
 rm *.md5
 rm *.gz
 cd /srv/shiny-server/PITFIT/data
-cd *
-cd READ
-cd *
+cd */READ/*
 for a in `ls -1 *.tar.gz`; do tar -zxvf $a; done
 rm *.md5
 rm *.gz
