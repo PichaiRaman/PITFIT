337b553e3e87cdacea559f3398a3df387370f9b5
PichaiRaman <pichai.raman@gmail.com>
2015-11-28 17:23:44 -0500

    Update createAim2_Robj.R

--- code/aim-2/createAim2_Robj.R
+++ code/aim-2/createAim2_Robj.R
@@ -4,8 +4,39 @@
 #11/28/2015
 ############################
 
-#First let's 
+#First let's start parse genemania data
 setwd("/home/ramanp/pitfit/data/GeneMania/genemania.org/data/current/Homo_sapiens");
+allFiles <-list.files();
+mappingFile <- read.delim("identifier_mappings.txt");
+mappingFile <- mappingFile[mappingFile[,3]=="Gene Name",1:2]
+
+interactionFilesType <- c("Physi", "Predi", "Genet", "Co-lo", "Co-ex");
+interactionFiles <- allFiles[substring(allFiles, 1,5)%in%interactionFilesType];
+
+interactionDF <- data.frame(read.delim(interactionFiles[1]), gsub(".txt", "", interactionFiles[1]))
+colnames(interactionDF)[4] <- "source"
+#for(i in 2:length(interactionFiles))
+for(i in 2:3)
+{
+intDFTmp <- data.frame(read.delim(interactionFiles[i], stringsAsFactors=F), gsub(".txt", "", interactionFiles[i]))
+colnames(intDFTmp)[4] <- "source"
+interactionDF <- rbind(interactionDF, intDFTmp);
+print(i/length(interactionFiles));
+}
+
+#Map it to ensembl
+interactionDF <- merge(interactionDF, mappingFile, by.x="Gene_A", by.y="Preferred_Name");
+interactionDF <- merge(interactionDF, mappingFile, by.x="Gene_B", by.y="Preferred_Name");
+interactionDF <- interactionDF[,c(5,6,3,4)];
+colnames(interactionDF)[1:2] <- c("Gene_A", "Gene_B");
+interactionDF[,"Type"] <- sapply(interactionDF[,"source"], FUN=pullOutStem);
+
+
+pullOutStem <- function(x)
+{
+myEnd <- gregexpr("\\.", x)[[1]][1]-1; 
+out <- substring(x, 1, myEnd);
+}
 
 
 
@@ -14,3 +45,7 @@ setwd("/home/ramanp/pitfit/data/GeneMania/genemania.org/data/current/Homo_sapien
 
 
 
+
+#"networks.txt"
+#"Pathway"
+#".gmt"
