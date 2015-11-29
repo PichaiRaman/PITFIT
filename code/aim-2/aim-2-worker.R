6b2adbf27206f8160f098851cf3e2dd093d592e6
PichaiRaman <pichai.raman@gmail.com>
2015-11-29 11:28:02 -0500

    Update aim-2-worker.R

--- code/aim-2/aim-2-worker.R
+++ code/aim-2/aim-2-worker.R
@@ -8,6 +8,31 @@
 
 #call libraries
 
+
+#Read & format druggability data#####################
+drugInt <- read.delim("../../data/categories.tsv", stringsAsFactors=F);
+druggable <- drugInt[drugInt[,"category"]=="DRUGGABLE GENOME",]
+rownames(druggable) <- druggable[,1];
+druggable[,"Drug_Score"] <- str_count(druggable[,"category_sources"], ",")+1
+druggable <- druggable[,c("entrez_gene_symbol", "category_sources", "Drug_Score")]
+colnames(druggable) <- c("Gene", "Sources_Druggability", "Score_Druggability");
+#####################################################
+
+
+
+
+#########Functions to add data and create score#############
+
+#Function to add druggability score
+isDruggable <- function(output)
+{
+tmpDruggable <- merge(output, druggable, by.x="Gene", by.y="Gene", all.x=T);
+tmpDruggable[is.na(tmpDruggable[,"Sources_Druggability"]),"Sources_Druggability"] <- "NONE";
+tmpDruggable[is.na(tmpDruggable[,"Score_Druggability"]),"Score_Druggability"] <- 0;
+return(tmpDruggable);
+}
+
+
 ##############################
 #Main Function to prioritize and rank
 #Targets
@@ -15,9 +40,10 @@
 ##############################
 pitfitAnalyzeAim2 <- function(myCancer, geneList)
 {
-
-output <- data.frame(geneList, myCancer);
-
+output <- data.frame(myCancer, geneList);
+colnames(output) <- c("Cancer", "Gene");
+#Add Druggability score to genes
+output <- isDruggable(output);
 return(output);
 }
 
