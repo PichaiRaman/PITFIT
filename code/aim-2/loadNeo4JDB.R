12450b7ac5ea5c401e4f022d2f551a33db4c031c
PichaiRaman <pichai.raman@gmail.com>
2016-02-06 19:05:27 -0500

    Create loadNeo4JDB.R

---
+++ code/aim-2/loadNeo4JDB.R
@@ -0,0 +1,16 @@
+###########################################
+#Code to load Neo4J
+#
+#
+###########################################
+
+#Install Neo4J first and make sure you have JDK8
+#http://tecadmin.net/install-java-8-on-centos-rhel-and-fedora/
+
+#using package RNeo4J
+#https://github.com/nicolewhite/RNeo4j
+#Please note when installing you may need to install openssldevel first  : yum install -y openssl-devel
+
+
+#Call library
+library(RNeo4j)
