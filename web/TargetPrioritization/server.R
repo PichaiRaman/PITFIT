20c9a67859c48cd770cd04b916ad5e02791b8a5e
PichaiRaman <pichai.raman@gmail.com>
2016-02-25 09:49:32 -0500

    Update server.R

--- web/TargetPrioritization/server.R
+++ web/TargetPrioritization/server.R
@@ -45,8 +45,8 @@ shinyServer(function(input, output, session) {
       # Pause for 0.1 seconds to simulate a long computation.
       Sys.sleep(0.1)
     }
-    myGenes <- strsplit(input$foo, "\t"); 
-  	myRes <- pitfitAnalyzeAim2(input$dataset,);
+    myGenes <- strsplit(input$foo, '\t')[[1]]; 
+  	myRes <- pitfitAnalyzeAim2(input$dataset,myGenes);
 globalResult <<- myRes; 
 myRes;
   })
