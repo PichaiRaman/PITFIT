d34b2b68d3e3bb46fc5893a47c3db9f8c38d3230
PichaiRaman <pichai.raman@gmail.com>
2016-02-26 02:12:11 +0000

    update worker

---
+++ code/aim-3/aim-3-worker.R
@@ -0,0 +1,16 @@
+##################################
+#Either find best set of lines for a single gene
+#or Greedy Algorithm to find the best set of lines
+#
+##################################
+
+#Read in the data
+
+
+
+
+
+
+
+
+
